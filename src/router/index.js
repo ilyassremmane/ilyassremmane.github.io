90162fe94bf059216675fae68174aa3f68969c8f
Remmane <remmane@MacBookPro.lan>
2026-03-06 14:56:23 +0100

    Correction de la page vide MesPhotosView

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,6 @@ const router = createRouter({
     { path: '/Rencontre', name: 'rencontre', component: () => import('../views/RencontreView.vue') },
     // Le ":id" veut dire que ce morceau d'URL peut changer (ex: /expo-2/marie-dupont)
     { path: '/Rencontre/:id', name: 'rencontre-detail', component: () => import('../views/RencontreDetailView.vue') },
-    { path: '/mes-photos', name: 'mes-photos', component: () => import('../views/MesPhotosView.vue') },
     { path: '/audiovisuel', name: 'audiovisuel', component: () => import('../views/AudioVisualView.vue') },
     { path: '/contact', name: 'contact', component: () => import('../views/ContactView.vue') },
     { path: '/portfolio', name: 'portfolio', component: () => import('../views/PortfolioView.vue') },
