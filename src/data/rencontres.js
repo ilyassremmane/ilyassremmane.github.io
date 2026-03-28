5826669912bd5bf7d9784c3a3921142889d92813
ilyassremmane <ilyass.remmane@edu.esiee.fr>
2026-03-29 00:28:48 +0100

    texte_v1

--- src/data/rencontres.js
+++ src/data/rencontres.js
@@ -4,7 +4,7 @@ export const listeRencontres = [
     id: 'renc_1.0', // L'identifiant unique qui sera dans l'URL (sans espace)
     nom: 'Abdelhafid',
     photoCouverture: '/photos/rencontre/renc_1.0.jpg', // Un portrait carré par exemple
-    texte: "J'ai rencontré...",
+    texte: "Je marchais un midi avec un appareil photo à la main en direction de l’ancienne ville d’Oujda (AL MDINA). Sur le trajet, mon regard croise celui d’Abdelhafid. Il fixe l’appareil, puis me regarde, je souris et je fais signe avec l’appareil. Un moment d’hésitation s’installe pour nous deux, mais sans m'en rendre compte, je me mets à prendre des photos. Il sourit, on parle, il invite ses amis/camarades à se faire prendre en photo. On parle, mais ils me parlent comme si j’étais un média, ils racontent leurs galères, les difficultés qu’ils ont à trouver des missions en tant qu’ouvriers journaliers.\n Un monsieur m’a dit : « Moi, ça fait 2 semaines que je n’ai pas de missions… » --- « هادي جوج سيمانات ماخدمت ...»",
     autresPhotos: [
       '/photos/rencontre/renc_1.1.jpg',
       '/photos/rencontre/renc_1.2.jpg',
@@ -16,7 +16,7 @@ export const listeRencontres = [
     id: 'renc_1.2',
     nom: 'Bagareur',
     photoCouverture: '/photos/rencontre/renc_2.0.jpg',
-    texte: "Sur le même trotoire...",
+    texte: "Dans la même rue, ou celle où il y avait Abdelhafid, plus j’avançais, plus j’avais d’interventions avec les gens. Beaucoup de personnes voulaient quelques billets en échange d’une photo que je prenais. Arrivé au bout de la rue, l’un du trio me dit : « Et nous, tu ne nous prends pas en photo ? » --- « و حنا ماتصورناش » Je ne réfléchis même pas, je prends des photos et je leur demande de s’approcher et de sourire. Le monsieur en veste de fourrure embête son pote pour lui enlever son cache-cou. Ça finit en bastounette mélangée à des câlins.",
     autresPhotos: [
       '/photos/rencontre/renc_2.1.jpg',
       '/photos/rencontre/renc_2.2.jpg'
@@ -26,7 +26,7 @@ export const listeRencontres = [
     id: 'renc_1.3',
     nom: 'Yeux',
     photoCouverture: '/photos/rencontre/renc_3.0.jpg',
-    texte: "Sur la grande place...",
+    texte: "En avançant pour arriver à ma destination, un corps léger et petit me bloque le chemin. Il me demande de l’argent tout en mangeant ses chips. Je lui ai dit : « Je n’en ai pas, mais si tu veux, je peux te prendre en photo » --- «معنديش، بصح إلا بغيت نصورك». À l’instant même où il entend ma phrase, il appelle son pote. Moi j’étais fasciné par sa pureté et sa beauté, dont la beauté de ses yeux ‘allah y7fdeh w ysakher fih’. Ils étaient tellement contents de se prendre en photo, et je l’étais bien sûr moi aussi. Je leur ai proposé d’essayer l’appareil, et à ce moment même, ils étaient plus que contents et j’étais pour une fois l’élément à photographier.",
     autresPhotos: [
       '/photos/rencontre/renc_3.1.jpg',
 
@@ -36,7 +36,7 @@ export const listeRencontres = [
     id: 'renc_2.0',
     nom: 'Un thé?',
     photoCouverture: '/photos/rencontre/renc_4.0.jpg',
-    texte: "Dans son kiosque   ...",
+    texte: "Dans cette journée d’été, le 16/07/2025. En début d’après-midi, je m’arrête avec mon pote dans cette belle et très propre plage à Kariat Arkmane. J’avais un appareil à la main car je réalisais des photos pour la série « Day_pic ». Le propriétaire de ce kiosque ainsi que son fils (je pense) pensaient que j’étais un média ou une chaîne de télévision. Donc on m’a parlé de la plage, du projet de parking qui devrait tout changer mais qui prend du temps à se réaliser. Puis au moment où nous voulions partir, il nous a proposé de rester pour boire un verre de thé.",
     autresPhotos: [
       '/photos/rencontre/renc_4.1.jpg',
       '/photos/rencontre/renc_4.2.jpg',
